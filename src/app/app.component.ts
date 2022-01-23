2be0b4efd09172d1f6116c07076a9218c8b31f33
Ozmar Cota <ozcota96@gmail.com>
2022-01-22 20:11:49 -0600

    ## master ### First commit #### Module 1 completed - Modified .gitignore to exclude png files - Modified app.component.html to remove initial template - Created product.model.ts to handle products - Modified app.module.ts to include new product interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'pedro'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'blablablabla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'blablablabla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'blablablabla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'blablablabla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Sticker 1',
+      price: 80000,
+      description: 'blablablabla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Sticker 2',
+      price: 80000,
+      description: 'blablablabla'
+    }
+  ];
+
+  addItem(){
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
 }
